512916403f213d8213fa668cfd2e828bea6bc1b4
Renat Vasilenko <64277973+mudaston@users.noreply.github.com>
2023-01-26 16:52:57 +0300

    change logic from array of filters to object of filters

--- src/redux/selectors/selectedFilters.ts
+++ src/redux/selectors/selectedFilters.ts
@@ -1,21 +1,13 @@
 import { createSelector } from '@reduxjs/toolkit'
 
 import type { RootState } from '../store'
-import { state } from '../store'
 
 const getSelectedFiltersState = (state: RootState) =>
-	state['selected-filters-slice'].elements
-
-/* const getSelectedFiltersByElementName = createSelector(
-	getSelectedFiltersState,
-	(filters) => filters.find((filter) => filter.name === 'departments')
-) */
+	state['selected-filters-slice']
 
 const getSelectedFiltersByElementName = (name: string) =>
-	createSelector(getSelectedFiltersState, (filters) => {
-		const hasFilter = filters.find((filter) => filter.name === name)
-
-		if (hasFilter) return hasFilter.filters
+	createSelector(getSelectedFiltersState, (state) => {
+		if (name in state.elements) return state.elements[name]
 
 		return []
 	})
