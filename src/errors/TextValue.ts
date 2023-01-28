a3326954891a97290b0a8964bc5fa882355f70c6
Renat Vasilenko <64277973+mudaston@users.noreply.github.com>
2023-01-28 23:56:21 +0200

    create errors for TextValue component

---
+++ src/errors/TextValue.ts
@@ -0,0 +1,6 @@
+const textComponentCannotBeEmpty =
+	'Cannot render TextValue component without value'
+
+const textComponentCanOnlyContainsText = 'Cannot render non-string value'
+
+export { textComponentCannotBeEmpty, textComponentCanOnlyContainsText }
