561214fc9408ee26026215efd4c040d566d8dded
Renat Vasilenko <64277973+mudaston@users.noreply.github.com>
2023-01-28 23:57:31 +0200

    create test for component

---
+++ src/components/atoms/TextValue/TextValue.test.jsx
@@ -0,0 +1,37 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+
+import TextValue from './TextValue'
+import Icon from '../Icon'
+
+import {
+	textComponentCanOnlyContainsText,
+	textComponentCannotBeEmpty,
+} from '../../../errors/TextValue'
+
+describe('render TextValue component', () => {
+	const text = 'Hello world'
+
+	it('render with value', async () => {
+		render(<TextValue>{text}</TextValue>)
+
+		expect(screen.getByText(text)).toBeInTheDocument()
+	})
+
+	it('render without value', async () => {
+		expect(() => render(<TextValue></TextValue>)).toThrowError(
+			textComponentCannotBeEmpty
+		)
+	})
+
+	it('render with non-string value', async () => {
+		expect(() =>
+			render(
+				<TextValue>
+					<Icon />
+				</TextValue>
+			)
+		).toThrowError(textComponentCanOnlyContainsText)
+	})
+})
